e269fdbc3c6ec09bf7556271c2b946296ea4da80
Lily Mansfield <lilymansfield@cs.uchicago.edu>
2021-02-15 19:26:48 -0600

    got subject area links, lined out tasks to complete for web crawler

--- PLOS_webcrawler.py
+++ PLOS_webcrawler.py
@@ -14,7 +14,8 @@ import csv
 
 def go(num_pages_to_crawl):
     '''
-    Crawl the PLOS One and generate a dictionary.
+    Crawl the PLOS One and generate a database. Automatically samples even
+    number of articles from each subject area based on num_pages_to_crawl.
 
     Inputs:
         num_pages_to_crawl: the number of pages to process during the crawl
@@ -22,20 +23,47 @@ def go(num_pages_to_crawl):
     Outputs:
         dictionary mapping author identifiers to list of variables
     '''
-
-    starting_url = ("https://journals.plos.org/plosone/browse#")
+    urls_visited = []
+    starting_url = ("https://journals.plos.org/plosone/browse")
     limiting_domain = "journals.plos.org"
+    request = util.get_request(starting_url) 
+
+    #get list of urls for subject areas
+    html_text = util.read_request(request)
+    current_url = util.get_request_url(request)  #gets correct url in case of redirection
+    soup = bs4.BeautifulSoup(html_text, 'html5lib')
+    soup_dropdown_menu_lst = soup.find_all("ul", typeof="v:Breadcrumb")
+    soup_subject_urls_lst = soup_dropdown_menu_lst[0].find_all("a")
+    subject_urls_lst = []
+    for a_tag in soup_subject_urls_lst[1:]:
+        url = util.convert_if_relative_url(current_url, a_tag["href"])
+        subject_urls_lst.append(url)
+
+    #iterate through subject areas
+    #for loop counting max urls per subject to crawl
+    #get all urls from page function => add to queue
+    #iterate through queue
+    #when queue empty get next page function
+    #decide urls to crawl per subject: num_pages_to_crawl/11.lower()
+    #
+
+
+
+
+
+
+
 
     url_queue = queue.Queue()
     urls_visited = []
     index = {}
     
-    request = util.get_request(starting_url)
+    request = util.get_request(starting_url) 
 
     for i in range(num_pages_to_crawl):
         if request:
             html_text = util.read_request(request)
-            current_url = util.get_request_url(request) 
+            current_url = util.get_request_url(request)  #gets correct url in case of redirection
             urls_visited.append(current_url)
             process_webpage(html_text, index, course_map)
             urls = get_relevant_urls(html_text, current_url, limiting_domain)
