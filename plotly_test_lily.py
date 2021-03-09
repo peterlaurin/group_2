b444f7f623bcceb4dbbf7731e0025047e2fa2e6c
Lily Mansfield <lilymansfield@cs.uchicago.edu>
2021-03-09 16:34:47 -0600

    plot tests

--- plotly_test_lily.py
+++ plotly_test_lily.py
@@ -23,11 +23,29 @@ df = pd.DataFrame(sql_query, columns = ['author_identifier', 'first_name', 'last
 #gender_country_df = df.groupby(['gender','country']).size()#creates a series
 
 df_gc = df.groupby(['gender','country']).size().to_frame('count').reset_index() #worked
+countries10 = df_gc[df_gc['count']>10].reset_index(drop=True)
+df = df.reset_index(drop=True) 
 
-fig = px.bar(grpd, x="country", y="count", color="gender", title="Long-Form Input")
+df_10c = df.merge(countries10['country'], on = 'country').reset_index(drop = True)
+grpd = df_10c.groupby(['gender','country']).size().to_frame('count').reset_index()
+
+# wrogn want perecent per country
+#total_count = grpd['count'].sum()
+#grpd['percent'] = grpd['count'].div(total_count)
+
+#df_10c.groupby(['gender'])
+
+ #df[df['country'] == countries10['country']] #Value Error
+
+fig = px.bar(grpd, x="country", y="percent", color="gender", title="Gender breakdown by countries with > 10 authors")
 fig.show()
 
-# countries with more than 10 entries
-df_10 = df.groupby(["country"]).apply(lambda x: x > 10)).reset_index(drop=True)
-# select top N rows within each continent
-g.groupby('continent').head(1)
+#only returned single col of 45 rows??
+
+#df['sales'] / df.groupby('state')['sales'].transform('sum')
+
+#https://stackoverflow.com/questions/23377108/pandas-percentage-of-total-with-groupby
+state_office = df.groupby(['state', 'office_id']).agg({'sales': 'sum'}) # Change: groupby state_office and divide by sum
+country_gender = grpd.groupby(['country']).agg({'count':'sum'})
+state_pcts = state_office.groupby(level=0).apply(lambda x: 100 * x / float(x.sum()))
+country_pcts = country_gender.groupby(level = 0).apply(lambda x: 100 * x / float(x.sum()))
