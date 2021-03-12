b15a24feda22091dcc723903ca3400bc23ed78d1
Peter Laurin <peterlaurin@cs.uchicago.edu>
2021-03-11 22:34:50 -0600

    cleaned journals.db more

--- database_cleaning.py
+++ database_cleaning.py
@@ -1,37 +1,59 @@
 import sqlite3
 
+CONN = sqlite3.connect('journals.db')
 
-def condense_US_entries(database_name):
+def country_not_found():
+    c = CONN.cursor()
+    q_str = "SELECT author_identifier, country FROM authors WHERE country = ''"
+    query = c.execute(q_str)
+    for author_id, country in query.fetchall():
+        new_q = 'UPDATE authors SET country = ? WHERE author_identifier = ?'
+        c.execute(new_q, ('country not found', author_id))
+        CONN.commit()
+
+def condense_country_entries():
     """
-    (Lily)
-    Rewrites iterations of United States in country column to "united states of america". Assumes "us" and "united states" refers
+    Rewrites iterations of United States in country column to "united states of
+    america". Assumes "us" and "united states" refers
     to United States of America. 
 
     Input:
         database_name (str): database name and filepath
     """
-    conn = sqlite3.connect(database_name)
-    c = conn.cursor()
-    iter_us = ("united sates of america", "united state of america", "united states", "united states america", "us", "usa")
-    lst_usa_entries = c.execute("SELECT author_identifier, country from authors WHERE country = ? OR country = ? OR country = ?\
-        OR country = ? OR country = ? OR country = ?", iter_us).fetchall()
+    c = CONN.cursor()
+    iter_us = ["united sates of america", "united state of america", 
+               "united states", "united states america", "us", "usa"]
+    real_countries = dict.fromkeys(iter_us, 'united states of america')
 
-    for author, country in lst_usa_entries:
-        c.execute('UPDATE authors SET country = ? WHERE author_identifer = ?', ('united states of america', author))
-    
+    real_countries.update({'aotearoa new zealand':'new zealand', 
+    'hong kong sar':'hong kong', 'p r china':'china', 'p. r. china':'china', 
+    'p.r. china':'china', "people's republic of china":'china', 
+    "people’s republic of china":'china', 'russian federation':'russia', 
+    'uk':'united kingdom', '':'country not found'})
 
-#replace empty country with "not found"?
+    q_bit = [' country = ? '] * len(real_countries.keys())
+    query = "SELECT author_identifier, country from authors WHERE" + \
+            'OR'.join(q_bit)
+    bad_country_query = c.execute(query, tuple(real_countries.keys()))
+
+    for author, bad_country in bad_country_query.fetchall():
+        c.execute('UPDATE authors SET country = ? WHERE author_identifier = ?'\
+                  , (real_countries[bad_country], author))
+        CONN.commit()
+
 
 def remove_the():
     '''
-    (Peter)
-    Removes 'the ' from institution in database in order to not double count institutions
+    Removes 'the ' from institution in database in order to not double 
+    count institutions
     '''
-    conn = sqlite3.connect('journals.db')
-    c = conn.cursor()
-    the_list = c.execute("SELECT author_identifier, institution from authors WHERE institution LIKE 'the %';")
+    c = CONN.cursor()
+    the_list = c.execute("SELECT author_identifier, institution from authors \
+                          WHERE institution LIKE 'the %';")
     the_list = the_list.fetchall()
     for author, inst in the_list:
         new_inst = inst[4:]
-        c.execute('UPDATE authors SET institution = ? WHERE author_identifier = ?', (new_inst, author))
-        conn.commit()
+        c.execute('UPDATE authors SET institution = ? \
+                   WHERE author_identifier = ?', (new_inst, author))
+        CONN.commit()
+
